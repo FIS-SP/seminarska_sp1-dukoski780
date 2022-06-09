5127ecd1fb39a60e0ead0e66f4f215f39e9a548e
dukoski780 <martin@dukoski.si>
2022-06-10 00:05:37 +0200

    Change product images, add add to cart button on hover on product box

--- assets/js/lanuma--main-script.js
+++ assets/js/lanuma--main-script.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
 
-  console.log("test");
+  console.log("Podatki brskalnika");
+  console.log("Cookies: " + navigator.cookieEnabled);
+  console.log("Browser Language: " + navigator.browserLanguage);
+  console.log("Language: " + navigator.language);
+  console.log("Platform: " + navigator.platform);
+  console.log("Connection Speed: " + navigator.connectionSpeed);
+  console.log("User Agent: " + navigator.userAgent);
+  console.log("Webdriver: " + navigator.webdriver);
+  console.log("Geolocation: " + navigator.geolocation);
+
+  function addToCartAlert() {
+    alert('Izdelek je "dodan v košarico"');
+    document.write ("Hvala");
+  }
  
   $('.method').on('click', function() {
     $('.method').removeClass('blue-border');
     $(this).addClass('blue-border');
   });
+
+  var $cardInput = $('.input-fields input');
+ 
+  $('.next-btn').on('click', function(e) {
+  
+    $cardInput.removeClass('warning');
+  
+    $cardInput.each(function() {    
+      var $this = $(this);
+      if (!$this.val()) {
+        $this.addClass('warning');
+      }
+    })
+  });
  
 })
-var $cardInput = $('.input-fields input');
- 
-$('.next-btn').on('click', function(e) {
- 
-  $cardInput.removeClass('warning');
- 
-  $cardInput.each(function() {    
-     var $this = $(this);
-     if (!$this.val()) {
-       $this.addClass('warning');
-     }
-  })
-});
